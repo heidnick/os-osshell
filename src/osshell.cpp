e1c51a5128905a40567454d2d8449634ea5d7fc6
heid7732@stthomas.edu <heid7732@stthomas.edu>
2021-02-28 14:28:54 -0600

    2/28

--- src/osshell.cpp
+++ src/osshell.cpp
@@ -5,6 +5,8 @@
 #include <sstream>
 #include <vector>
 #include <unistd.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
 
 void splitString(std::string text, char d, std::vector<std::string>& result);
 void vectorOfStringsToArrayOfCharArrays(std::vector<std::string>& list, char ***result);
@@ -16,21 +18,6 @@ int main (int argc, char **argv)
     std::vector<std::string> os_path_list;
     char* os_path = getenv("PATH");
     splitString(os_path, ':', os_path_list);
-
-    
-    /************************************************************************************
-     *   Example code - remove in actual program                                        *
-     ************************************************************************************/
-    // Shows how to loop over the directories in the PATH environment variable
-    int i;
-    for (i = 0; i < os_path_list.size(); i++)
-    {
-        printf("PATH[%2d]: %s\n", i, os_path_list[i].c_str());
-    }
-    /************************************************************************************
-     *   End example code                                                               *
-     ************************************************************************************/
-
 
     // Welcome message
     printf("Welcome to OSShell! Please enter your commands ('exit' to quit).\n");
@@ -45,45 +32,75 @@ int main (int argc, char **argv)
     //  For all other commands, check if an executable by that name is in one of the PATH directories
     //   If yes, execute it
     //   If no, print error statement: "<command_name>: Error command not found" (do include newline)
-
-
-    /************************************************************************************
-     *   Example code - remove in actual program                                        *
-     ************************************************************************************/
-    // Shows how to split a command and prepare for the execv() function
-    std::string example_command = "ls -lh";
-    splitString(example_command, ' ', command_list);
+    std::cout << "osshell> ";
+    char usr_command[100];
+    std::cin.getline(usr_command, sizeof(usr_command));
+    std::cout << usr_command << std::endl;
+    
+    //std::string example_command = "ls -lh";
+    //splitString(example_command, ' ', command_list);
+    splitString(usr_command, ' ', command_list);
     vectorOfStringsToArrayOfCharArrays(command_list, &command_list_exec);
-    // use `command_list_exec` in the execv() function rather than looping and printing
-    i = 0;
-    while (command_list_exec[i] != NULL)
+
+    /*
+        cur_path   -> store env path, must add .c_str() when using cur_path as param for stat()
+        buf        -> pass as &buf as second param to stat()
+        found_path -> if 0, no path contains executable file, 1 vis versa
+        pid        -> if pid > 0 = parent, if pid = 0 = child
+    */
+
+    std::string cur_path;
+    struct stat buf;
+    int found_path = 0;
+    pid_t pid;
+    
+    //Uncomment to print cmd line
+    /*
+    int j = 0;
+    while (command_list_exec[j] != NULL)
     {
-        printf("CMD[%2d]: %s\n", i, command_list_exec[i]);
-        i++;
+        std::cout << command_list_exec[j] << std::endl;
+        j++;
     }
-    // free memory for `command_list_exec`
-    freeArrayOfCharArrays(command_list_exec, command_list.size() + 1);
-    printf("------\n");
+    */
 
-    // Second example command - reuse the `command_list` and `command_list_exec` variables
-    example_command = "echo \"Hello world\" I am alive!";
-    splitString(example_command, ' ', command_list);
-    vectorOfStringsToArrayOfCharArrays(command_list, &command_list_exec);
-    // use `command_list_exec` in the execv() function rather than looping and printing
-    i = 0;
-    while (command_list_exec[i] != NULL)
-    {
-        printf("CMD[%2d]: %s\n", i, command_list_exec[i]);
-        i++;
+    /*
+        Done:
+        Parse through env paths, appends '/filename' to cur_path
+        if stat() returns valid, set found_path to true and fork()
+        if inside child process, execv to execute user process.  ---> probably need error check for bad user params
+
+        To Do:
+        - error check for bad user params on execv
+        - create loop to reprompt user
+        - exit -> check if command_list_exec[0] is exit, then call exit(1)
+        - history -> create a history size 128 string array
+        - create printHist(int start)
+        - in same error check as exit, check for ./ executable files
+        - readHist() writeHist() functions for saving/reading from file
+        - what happens when we get new commands? probably need to keep an index of oldest/most recent for history array
+        - on history clear, should initialize all to NULL?
+    */
+    for (int i = 0; i < os_path_list.size(); i++){
+        cur_path = os_path_list[i].c_str();
+        cur_path += "/";
+        cur_path += command_list_exec[0];
+        if (stat(cur_path.c_str(), &buf) == 0 && found_path == 0) {
+            found_path = 1;
+            std::cout << cur_path << std::endl;
+            pid = fork();
+            if (pid == 0) {
+                printf("child spawned\n");
+                execv(cur_path.c_str(), command_list_exec);
+                exit(0);
+            }
+            else if (pid > 0) {
+                wait(NULL);
+            }
+        }
     }
-    // free memory for `command_list_exec`
     freeArrayOfCharArrays(command_list_exec, command_list.size() + 1);
-    printf("------\n");
-    /************************************************************************************
-     *   End example code                                                               *
-     ************************************************************************************/
-
-
+    
     return 0;
 }
 
